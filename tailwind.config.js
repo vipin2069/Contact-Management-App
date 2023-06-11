eda40174087832ce0e3f9b275f488a07cb7ba3b4
Vipin Kumar Bhasker <vipin20699@gmail.com>
2023-06-12 01:23:54 +0530

    fsdf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+// /** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js,tsx}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
